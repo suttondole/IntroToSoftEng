b4cfcac95522beb25a515552ce01b943c190fea7
Sutton Dole <sdole@stevens.edu>
2021-02-21 16:37:59 -0500

    adding first half of lecture 3 and adjusting p3

--- p3-sdole.py
+++ p3-sdole.py
@@ -1,14 +1,19 @@
 """Sutton Dole's P3
    User defined functions
+   Returning the max of three numbers
 """
 
 def maxOfThree():
-    first = input('Enter first value: ')
-    second = input('Enter second value: ')
-    third = input('Enter third value: ')
-    maxi = first
-    if maxi < second:
-        maxi = second
-    if maxi < third:
-        maxi = third
-    print("The max of " + first + " " + second + " " + third + " is " + maxi)
+    try:
+        first = float(input('Enter first value: '))
+        second = float(input('Enter second value: '))
+        third = float(input('Enter third value: '))
+        maxi = first
+        if maxi < second:
+            maxi = second
+        if maxi < third:
+            maxi = third
+        print("The max of " + str(int(first)) + " " + str(int(second)) + " " + str(int(third)) + " is " + str(int(maxi)))
+    except:
+        print("Please enter a number")
+    
