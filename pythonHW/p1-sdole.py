f686339cda798e7e0e1aae1798da36f1b8f41c48
Sutton Dole <sdole@stevens.edu>
2021-02-24 20:17:23 -0500

    organizing python scripts into a folder

---
+++ pythonHW/p1-sdole.py
@@ -0,0 +1,13 @@
+"""Sutton Dole's P1
+   Introduction to Python
+   Greeting someone based on their input
+"""
+
+#declaring variables for first and last name and assigning their respective values using the user input
+print("Please tell me your first name: ")
+fName = input()
+print("Please tell me your last name: ")
+lName = input()
+#string to output the greeting
+print("Hello " + fName + " " + lName)
+    
